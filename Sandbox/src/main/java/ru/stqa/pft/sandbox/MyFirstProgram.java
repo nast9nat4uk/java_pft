e9d50dea3ea6de1775a0300650512c2988862c01
Nastya Natchuk <nast9nat4uk@gmail.com>
2017-02-12 19:56:43 +0300

    Созданы классы Rectangle и Square

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,13 +5,12 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         hello("world");
 
-        double l=5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        Square s = new Square(5);
 
-        double a=4;
-        double b=6;
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        System.out.println("Площадь прямоугольника со сторонами " + a + "и " + b +  " = " + area(a,b));
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + "и " + r.b +  " = " + r.area());
 
     }
 
@@ -20,11 +19,7 @@ public class MyFirstProgram {
         System.out.println("Hello," + somebody +  "!");
     }
 
-    public  static double area(double len){
-        return len*len;
-    }
 
-    public  static double area (double a, double b){
-        return a*b;
-    }
+
+
 }
---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Настя on 12.02.2017.
+ */
+public class Square {
+    public double l;
+
+    public Square (double l){
+        this.l=l;
+    }
+
+    public   double area(){
+        return this.l*this.l;
+    }
+}
