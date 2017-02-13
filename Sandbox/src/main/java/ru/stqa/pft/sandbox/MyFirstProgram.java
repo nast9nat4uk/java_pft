1b62de2bdd2ff068637cc13056823d8a3cf0f162
Nastya Natchuk <nast9nat4uk@gmail.com>
2017-02-13 20:21:26 +0300

    В класс Point добавлен метод distance

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,29 +11,18 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(4, 6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + "и " + r.b + " = " + r.area());
 
-        Point p1 = new Point(3,2);
-        Point p2 = new Point (5,6);
-        System.out.println("Расстояние между точками x и " + "y равно " + distance(p1, p2));
+        Point p1 = new Point(3, 2);
+        Point p2 = new Point(5, 6);
+        System.out.println("Расстояние между точками p1(" + p1.x + ";" + p1.y + ") и p2(" + +p2.x + ";" + p2.y + ") равно " + p1.distance(p2));
 
 
     }
 
-
-    public static double distance(Point p1, Point p2) {
-
-        double sumx=(p1.x-p2.x)*(p1.x-p2.x); // квадрат разности координат x
-        double sumy = (p1.y-p2.y)*(p1.y-p2.y); //квадрат разности координат Y
-        double sum = sumx+sumy;
-        return  Math.sqrt(sum);
-
-    }
 
     public static void hello(String somebody) {
 
         System.out.println("Hello," + somebody + "!");
     }
-
-
 
 
 }
--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,12 +8,16 @@ public class Point {
     public double x;
     public double y;
 
-    public Point(double x, double y){
+    public Point(double x, double y) {
 
-        this.x=x;
-        this.y=y;
+        this.x = x;
+        this.y = y;
 
     }
 
+    public double distance(Point p2) {
 
+        return Math.sqrt((this.x - p2.x) * (this.x - p2.x) + (this.y - p2.y) * (this.y - p2.y));
+
+    }
 }
