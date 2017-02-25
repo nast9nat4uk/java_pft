b5c2c17e8b0fc450ecba1ccd4a3d7bdf3cf7d8b7
Nastya Natchuk <nast9nat4uk@gmail.com>
2017-02-25 20:16:49 +0300

    Добавлена возможность запуска в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import sun.plugin2.util.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,14 +13,27 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationManager {
 
-    FirefoxDriver wd;
+    WebDriver wd;
     private  NavigationHelper navigationHelper;
     private  UserHelper userHelper;
     private  GroupHelper groupHelper;
     private  SessionHelper sessionHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
 
     public void init() {
+
+        if (browser== org.openqa.selenium.remote.BrowserType.FIREFOX){
+            wd = new FirefoxDriver();
+        } else if (browser== org.openqa.selenium.remote.BrowserType.CHROME){
+            wd = new ChromeDriver();
+        } else if (browser == org.openqa.selenium.remote.BrowserType.IE){
+            wd = new InternetExplorerDriver();
+        }
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -1,13 +1,14 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by Настя on 19.02.2017.
  */
 public class NavigationHelper extends HelperBase{
-    public NavigationHelper(FirefoxDriver wd) {
+    public NavigationHelper(WebDriver wd) {
         super(wd);
     }
 
