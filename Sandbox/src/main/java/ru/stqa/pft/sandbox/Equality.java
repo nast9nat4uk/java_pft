3aae886d251249f20a150d246aff7b80e7572360
Nastya Natchuk <nast9nat4uk@gmail.com>
2017-02-27 14:55:41 +0300

    Поля по умолчанию остаются заполненными, проверка совпадения вводимого значения с уже существующим

---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Equality.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Настя on 26.02.2017.
+ */
+public class Equality {
+    public  static  void main(String [] args){
+        String s1="firefox";
+        String  s2 = new String (s1);
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+    }
+}
