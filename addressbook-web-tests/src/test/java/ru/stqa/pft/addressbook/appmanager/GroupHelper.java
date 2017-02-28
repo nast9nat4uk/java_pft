8606d2994c1eb91e5fc67d0c20b280b52cba012d
Nastya Natchuk <nast9nat4uk@gmail.com>
2017-02-28 14:28:21 +0300

    Проверка и обеспечение выполнения предусловий добавлена

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -51,4 +51,16 @@ public class GroupHelper extends HelperBase {
     public void confirmAlert() {
         wd.switchTo().alert().accept();
     }
+
+    public void createGroup(GroupData group) {
+
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    public boolean isThereAGroup() {
+        return isElementPresent(By.name("selected[]"));
+    }
 }
