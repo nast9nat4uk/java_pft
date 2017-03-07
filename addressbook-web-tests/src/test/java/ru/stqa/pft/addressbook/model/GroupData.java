0550a59d4458f7cc0f4bb94571aa70c0a212a6d6
Nastya Natchuk <nast9nat4uk@gmail.com>
2017-03-07 14:24:48 +0300

    Добавено сравнение списков дл GropCreationTest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,25 +4,28 @@ public class GroupData {
     private final String name;
     private final String header;
     private final String footer;
+    private int id;
 
 
-
-    private final String id;
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
+
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
@@ -39,29 +42,29 @@ public class GroupData {
     }
 
     @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         GroupData groupData = (GroupData) o;
 
-        if (name != null ? !name.equals(groupData.name) : groupData.name != null) return false;
-        return id != null ? id.equals(groupData.id) : groupData.id == null;
+        if (id != groupData.id) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
     }
 
     @Override
     public int hashCode() {
         int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (id != null ? id.hashCode() : 0);
+        result = 31 * result + id;
         return result;
-    }
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                '}';
     }
 
 }
