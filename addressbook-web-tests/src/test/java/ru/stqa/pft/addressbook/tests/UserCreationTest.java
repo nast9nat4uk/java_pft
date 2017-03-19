897deeb00d93a9d171ce8e49e619c1a3d0fe7784
Nastya Natchuk <nast9nat4uk@gmail.com>
2017-03-19 20:35:49 +0300

    параметризованы тесты для групп и пользователей

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreationTest.java
@@ -1,23 +1,35 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.UserData;
 import ru.stqa.pft.addressbook.model.Users;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 public class UserCreationTest extends TestBase {
 
+    @DataProvider
+    public Iterator<Object[]> validUsers(){
+        List<Object[]> list =  new ArrayList<Object[]>();
+        list.add(new Object[] {new UserData().withName("name1").withLastName("lastname1")});
+        list.add(new Object[] {new UserData().withName("name2").withLastName("lastname2")});
+        list.add(new Object[] {new UserData().withName("name3").withLastName("lastname3")});
+        return list.iterator();
+    }
 
-    @Test
-    public void testAddUser() {
 
+    @Test (dataProvider = "validUsers")
+    public void testAddUser(UserData user) {
         Users before = app.user().all();
         File photo = new File("src/test/resources/1.jpg");
-        UserData user = new UserData().withName("nameTest333").withLastName("Name2Test").withPhoto(photo);
         app.user().create(user);
         assertThat(app.user().count(), equalTo(before.size()+1));
         Users after = app.user().all();
