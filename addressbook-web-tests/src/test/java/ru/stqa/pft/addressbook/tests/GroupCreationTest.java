4e6647e3ea50531cb6501022840cf45ac430cfd4
Nastya Natchuk <nast9nat4uk@gmail.com>
2017-03-24 16:06:01 +0300

    протоколирование с помощью logback

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -22,6 +24,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class GroupCreationTest extends TestBase {
+
+
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
@@ -58,6 +62,7 @@ public class GroupCreationTest extends TestBase {
     }
     @Test (dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) {
+
         app.goTo().groupPage();
         Groups before = app.group().all();
         app.group().create(group);
@@ -65,6 +70,7 @@ public class GroupCreationTest extends TestBase {
         Groups after = app.group().all();
         assertThat(after, equalTo
                 (before.withAdded(group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt()))));
+
     }
 
     @Test
