f0bb33e8a1a6e7320180f3fdff75e7949e1f1e9f
Nastya Natchuk <nast9nat4uk@gmail.com>
2017-03-24 15:06:10 +0300

    Создан конфигурационный файл для запуска тестов с заданными параметрами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -8,6 +8,11 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -15,6 +20,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationManager {
 
+    private final Properties properties;
     WebDriver wd;
     private  NavigationHelper navigationHelper;
     private  UserHelper userHelper;
@@ -22,14 +28,16 @@ public class ApplicationManager {
     private  SessionHelper sessionHelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser){
         this.browser = browser;
+        properties = new Properties();
     }
 
 
-    public void init() {
+    public void init() throws IOException {
 
-
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
         if (browser.equals(BrowserType.FIREFOX)){
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)){
@@ -40,14 +48,14 @@ public class ApplicationManager {
         }
 
         wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook");
+        wd.get(properties.getProperty("web.baseUrl"));
 
 
         groupHelper = new GroupHelper(wd);
         userHelper = new UserHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
 
     }
 
