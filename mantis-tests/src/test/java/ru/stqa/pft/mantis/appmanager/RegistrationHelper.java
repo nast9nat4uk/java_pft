8e96d97454e724e7172e4a258c5fb80646ef7453
Nastya Natchuk <nast9nat4uk@gmail.com>
2017-04-06 20:01:58 +0300

    Работа с почтовым сервером.
    Тест для регистрации нового пользователя

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,21 +1,32 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import com.sun.javafx.binding.ExpressionHelperBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 /**
  * Created by Настя on 03.04.2017.
  */
-public class RegistrationHelper {
-    private final ApplicationManager app;
+public class RegistrationHelper extends HelperBase  {
     private WebDriver wd;
 
     public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
+        super(app);
         wd = app.getDriver();
     }
 
     public void start(String username, String email) {
 
         wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+        type(By.name("username"),username);
+        type(By.name("email"), email);
+        click((By.cssSelector("input[value='Signup']")));
+    }
+
+    public void finish(String confirmationLink, String password) {
+        wd.get(confirmationLink);
+        type(By.name("password"),password);
+        type(By.name("password_confirm"),password);
+        click((By.cssSelector("input[value='Update User']")));
     }
 }
