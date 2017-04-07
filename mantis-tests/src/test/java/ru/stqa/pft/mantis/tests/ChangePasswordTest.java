2af6aa23fc9ad475ecfe2e47ad3a5b3dbd091906
Nastya Natchuk <nast9nat4uk@gmail.com>
2017-04-07 20:36:42 +0300

    07/04

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTest.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTest.java
@@ -1,19 +1,46 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import java.util.List;
 
 /**
  * Created by Настя on 07.04.2017.
  */
-public class ChangePasswordTest extends  TestBaseForHometask {
+public class ChangePasswordTest extends TestBase {
 
+    @BeforeMethod
+    public void startMailServer() {
+        app.mail().start();
+    }
 
     @Test
-    public  void changePasswordTest(){
-    app.getDriver();
-    app.perform().login("administrator", "root");
-    app.perform().resetPassword("user1");
+    public void changePasswordTest() {
+        app.getDriver();
+        app.changePassword().login("administrator", "root");
+        String username = "user1";
+        String email = username + "@localhost.localdomain";
+        app.changePassword().resetPassword(username);
+        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        String confirmationLink = findConfirmationLink(mailMessages, email);
+        System.out.println(confirmationLink);
+        //app.changePassword().finish(confirmationLink, "newpassword");
+
 
     }
 
+    private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+        MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void stopMailServer() {
+        app.mail().stop();
+    }
 }
